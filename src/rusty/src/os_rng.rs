cd147355ebf93c96ee14fe19adec2cc56855bf86
James Lovejoy <jameslovejoy1@gmail.com>
2019-10-25 14:24:59 -0400

    Compilation working

--- src/rusty/src/os_rng.rs
+++ src/rusty/src/os_rng.rs
@@ -2,8 +2,11 @@ extern crate getrandom;
 
 #[no_mangle]
 pub extern "C" fn get_secure_random_uint32() -> u32 {
-    let mut buf = [0u8; 32];
-    getrandom::getrandom(&mut buf)?;
+    let mut buf = [0u8; 4];
+    getrandom::getrandom(&mut buf).unwrap();
 
-    buf[0] as u32
+    ((buf[0] as u32) << 24) |
+    ((buf[1] as u32) << 16) |
+    ((buf[2] as u32) << 8) |
+    buf[3] as u32
 }
